7ff4bffa6a20659625d5ce2fdb57441ba2edcce3
atanasderekyuvliev18b@gmail.com <atanasderekyuvliev18b@gmail.com>
2023-05-08 14:05:40 +0300

    test1

---
+++ SystemDB.Plugins.EFCore/Migrations/20230508105436_Unit2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SystemDB.Plugins.EFCore.Migrations
+{
+    public partial class Unit2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
